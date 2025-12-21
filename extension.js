19065e0af95831cda88df39d57e07fd523251f38
fthx <23138504+fthx@users.noreply.github.com>
2025-12-21 08:44:31 +0100

    New cleaner code + GS 49

--- extension.js
+++ extension.js
@@ -1,13 +1,12 @@
 /*
     Bottom Overview
     GNOME Shell 45+ extension
-    Copyright @fthx 2025
-    Adapted from @jdoda's Hot Edge and GNOME Shell's layout.js
-    License GPL v3
+    @fthx 2025
 */
 
 
 import Clutter from 'gi://Clutter';
+import GLib from 'gi://GLib';
 import GObject from 'gi://GObject';
 import Meta from 'gi://Meta';
 import Shell from 'gi://Shell';
@@ -16,76 +15,90 @@ import * as Layout from 'resource:///org/gnome/shell/ui/layout.js'
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 
 
+const PRESSURE_THRESHOLD = 150; // px
 const HOT_EDGE_PRESSURE_TIMEOUT = 1000; // ms
-const PRESSURE_TRESHOLD = 150;
-const EDGE_SIZE = 100; // %
-
 
 const BottomOverview = GObject.registerClass(
-class BottomOverview extends Clutter.Actor {
-    _init(monitor, x, y) {
-        super._init();
+    class BottomOverview extends Clutter.Actor {
+        _init() {
+            super._init();
 
-        this._monitor = monitor;
-        this._x = x;
-        this._y = y;
+            this._initPressureBarrier();
+            this._setHotEdges();
 
-        this._edgeSize = EDGE_SIZE / 100;
-        this._pressureThreshold = PRESSURE_TRESHOLD;
-
-        this._pressureBarrier = new Layout.PressureBarrier(
-            this._pressureThreshold,
-            HOT_EDGE_PRESSURE_TIMEOUT,
-            Shell.ActionMode.NORMAL | Shell.ActionMode.OVERVIEW);
-
-        this._pressureBarrier?.connectObject('trigger', this._toggleOverview.bind(this), this);
-        this.connectObject('destroy', this._destroy.bind(this), this);
-    }
-
-    setBarrierSize(size) {
-        if (this._barrier) {
-            this._pressureBarrier?.removeBarrier(this._barrier);
-            this._barrier.destroy();
-            this._barrier = null;
+            Main.layoutManager.connectObject('hot-corners-changed', () => this._setHotEdges(), this);
         }
 
-        if (size > 0) {
-            size = this._monitor.width * this._edgeSize;
-            let x_offset = (this._monitor.width - size) / 2;
+        _initPressureBarrier() {
+            this._pressureBarrier = new Layout.PressureBarrier(
+                PRESSURE_THRESHOLD,
+                HOT_EDGE_PRESSURE_TIMEOUT,
+                Shell.ActionMode.NORMAL | Shell.ActionMode.OVERVIEW);
 
-            this._barrier = new Meta.Barrier({
-                backend: global.backend,
-                x1: this._x + x_offset, x2: this._x + x_offset + size,
-                y1: this._y, y2: this._y,
-                directions: Meta.BarrierDirection.NEGATIVE_Y});
-            this._pressureBarrier?.addBarrier(this._barrier);
+            this._pressureBarrier?.connectObject('trigger', () => Main.overview.toggle(), this);
         }
-    }
 
-    vfunc_leave_event(event) {
-        return Clutter.EVENT_PROPAGATE;
-    }
+        _setBarriers() {
+            const monitors = Main.layoutManager.monitors;
 
-    _toggleOverview() {
-        if (Main.overview.shouldToggleByCornerOrButton()
-                && !(global.get_pointer()[2] & Clutter.ModifierType.BUTTON1_MASK)
-                && !this._monitor.inFullscreen)
-            Main.overview.toggle();
-    }
+            for (const monitor of monitors) {
+                const barrier = new Meta.Barrier({
+                    backend: global.backend,
+                    x1: monitor.x,
+                    y1: monitor.y + monitor.height,
+                    x2: monitor.x + monitor.width,
+                    y2: monitor.y + monitor.height,
+                    directions: Meta.BarrierDirection.NEGATIVE_Y
+                });
 
-    _destroy() {
-        this.setBarrierSize(0);
+                this._pressureBarrier?.addBarrier(barrier);
+            }
+        }
 
-        this._pressureBarrier?.disconnectObject(this);
-        this._pressureBarrier?.destroy();
-        this._pressureBarrier = null;
+        _setHotEdges() {
+            this._destroyBarriers();
 
-        super.destroy();
-    }
-});
+            if (this._hotEdgeTimeout)
+                GLib.Source.remove(this._hotEdgeTimeout);
+
+            this._hotEdgeTimeout = GLib.idle_add(GLib.PRIORITY_DEFAULT, () => {
+                this._setBarriers();
+
+                this._hotEdgeTimeout = null;
+                return GLib.SOURCE_REMOVE;
+            });
+        }
+
+        _destroyBarriers() {
+            for (let barrier of this._pressureBarrier?._barriers) {
+                this._pressureBarrier?.removeBarrier(barrier);
+                barrier?.destroy();
+                barrier = null;
+            }
+        }
+
+        _destroyPressureBarrier() {
+            this._pressureBarrier?.disconnectObject(this);
+            this._pressureBarrier?.destroy();
+            this._pressureBarrier = null;
+        }
+
+        destroy() {
+            if (this._hotEdgeTimeout) {
+                GLib.Source.remove(this._hotEdgeTimeout);
+                this._hotEdgeTimeout = null;
+            }
+
+            Main.layoutManager.disconnectObject(this);
+            this._destroyBarriers();
+            this._destroyPressureBarrier();
+
+            super.destroy();
+        }
+    });
 
 export default class BottomOverviewExtension {
-    _updateHotEdges() {
+    /*_updateHotEdges() {
         Main.layoutManager._destroyHotCorners();
 
         for (let i = 0; i < Main.layoutManager.monitors.length; i++) {
@@ -116,15 +129,14 @@ export default class BottomOverviewExtension {
             } else
                 Main.layoutManager.hotCorners.push(null);
         }
-    }
+    }*/
 
     enable() {
-        Main.layoutManager.connectObject('hot-corners-changed', this._updateHotEdges.bind(this), this);
-        this._updateHotEdges();
+        this._bottomOverview = new BottomOverview();
     }
 
     disable() {
-        Main.layoutManager.disconnectObject(this);
-        Main.layoutManager._updateHotCorners();
+        this._bottomOverview?.destroy();
+        this._bottomOverview = null;
     }
 }
