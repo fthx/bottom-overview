d5eba5027d4651638bed81a533424c7e6e4763cc
fthx <23138504+fthx@users.noreply.github.com>
2026-08-23 18:32:59 +0200

    Allow dnd through overview
    E.g. dnd a file from a Nautilus window to another.

--- extension.js
+++ extension.js
@@ -7,136 +7,123 @@
 
 import Clutter from 'gi://Clutter';
 import GLib from 'gi://GLib';
-import GObject from 'gi://GObject';
 import Meta from 'gi://Meta';
 import Shell from 'gi://Shell';
 
-import * as Layout from 'resource:///org/gnome/shell/ui/layout.js'
+import * as Layout from 'resource:///org/gnome/shell/ui/layout.js';
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 
 
 const PRESSURE_THRESHOLD = 150; // px
 const HOT_EDGE_PRESSURE_TIMEOUT = 1000; // ms
 
-const BottomOverview = GObject.registerClass(
-    class BottomOverview extends Clutter.Actor {
-        _init() {
-            super._init();
 
-            this._initPressureBarrier();
-            this._setHotEdges();
+class BottomOverview {
+    constructor() {
+        this._initPressureBarrier();
+        this._setHotEdges();
 
-            Main.layoutManager.connectObject('monitors-changed', () => this._setHotEdges(), this);
-        }
-
-        _initPressureBarrier() {
-            this._pressureBarrier = new Layout.PressureBarrier(
-                PRESSURE_THRESHOLD,
-                HOT_EDGE_PRESSURE_TIMEOUT,
-                Shell.ActionMode.NORMAL | Shell.ActionMode.OVERVIEW);
-
-            this._pressureBarrier?.connectObject('trigger', () => this._toggleOverview(), this);
-        }
-
-        _toggleOverview() {
-            const monitor = Main.layoutManager.primaryMonitor;
-
-            if (Main.overview.shouldToggleByCornerOrButton()
-                && !(global.get_pointer()[2] & Clutter.ModifierType.BUTTON1_MASK)
-                && !monitor?.inFullscreen)
-                Main.overview.toggle();
-        }
-
-        _setHotEdges() {
-            this._destroyBarriers();
-
-            if (this._hotEdgesTimeout)
-                GLib.Source.remove(this._hotEdgesTimeout);
-
-            this._hotEdgesTimeout = GLib.idle_add(GLib.PRIORITY_DEFAULT, () => {
-                this._setBarriers();
-
-                this._hotEdgesTimeout = null;
-                return GLib.SOURCE_REMOVE;
-            });
-        }
-
-        _setBarriers() {
-            const monitors = Main.layoutManager.monitors;
-
-            if (!monitors)
-                return;
-
-            for (const monitor of monitors) {
-                const { width: width, height: height, x, y } = monitor;
-
-                let hasBottom = true;
-
-                for (const otherMonitor of monitors) {
-                    if (!otherMonitor || otherMonitor === monitor)
-                        continue;
-
-                    if (otherMonitor.y >= y + height
-                        && otherMonitor.x < x + width
-                        && otherMonitor.x + otherMonitor.width > x)
-                        hasBottom = false;
-                }
-
-                if (hasBottom) {
-                    const barrier = new Meta.Barrier({
-                        backend: global.backend,
-                        x1: x,
-                        y1: y + height,
-                        x2: x + width,
-                        y2: y + height,
-                        directions: Meta.BarrierDirection.NEGATIVE_Y
-                    });
-
-                    this._pressureBarrier?.addBarrier(barrier);
-                }
-            }
-        }
-
-        _destroyBarriers() {
-            while (this._pressureBarrier?._barriers.length > 0) {
-                const barrier = this._pressureBarrier?._barriers[0];
-
-                this._pressureBarrier?.removeBarrier(barrier);
-                barrier.destroy();
-            }
-        }
-
-        _destroyPressureBarrier() {
-            this._pressureBarrier?.disconnectObject(this);
-            this._pressureBarrier?.destroy();
-            this._pressureBarrier = null;
-        }
-
-        destroy() {
-            if (this._hotEdgesTimeout) {
-                GLib.Source.remove(this._hotEdgesTimeout);
-                this._hotEdgesTimeout = null;
-            }
-
-            Main.layoutManager.disconnectObject(this);
-
-            this._destroyBarriers();
-            this._destroyPressureBarrier();
-
-            super.destroy();
-        }
-    });
-
-export default class BottomOverviewExtension {
-    _initBottomOverview() {
-        this._bottomOverview = new BottomOverview();
+        Main.layoutManager.connectObject('monitors-changed', () => this._setHotEdges(), this);
     }
 
+    _initPressureBarrier() {
+        this._pressureBarrier = new Layout.PressureBarrier(
+            PRESSURE_THRESHOLD,
+            HOT_EDGE_PRESSURE_TIMEOUT,
+            Shell.ActionMode.NORMAL | Shell.ActionMode.OVERVIEW);
+
+        this._pressureBarrier.connectObject('trigger', () => this._toggleOverview(), this);
+    }
+
+    _toggleOverview() {
+        const monitor = Main.layoutManager.primaryMonitor;
+
+        if (Main.overview.shouldToggleByCornerOrButton()
+            && (Main.overview._inXdndDrag
+                || !(global.get_pointer()[2] & Clutter.ModifierType.BUTTON1_MASK))
+            && !monitor?.inFullscreen)
+            Main.overview.toggle();
+    }
+
+    _setHotEdges() {
+        this._destroyBarriers();
+
+        if (!this._barriersTimeout)
+            this._barriersTimeout = GLib.idle_add_once(GLib.PRIORITY_DEFAULT_IDLE, () => {
+                this._setBarriers();
+
+                this._barriersTimeout = null;
+            });
+    }
+
+    _setBarriers() {
+        if (!this._pressureBarrier)
+            return;
+
+        const monitors = Main.layoutManager.monitors;
+        if (!monitors)
+            return;
+
+        for (const monitor of monitors) {
+            const { width, height, x, y } = monitor;
+
+            const hasBottom = !monitors.some(other =>
+                other !== monitor
+                && other.y >= y + height
+                && other.x < x + width
+                && other.x + other.width > x);
+
+            if (hasBottom) {
+                const barrier = new Meta.Barrier({
+                    backend: global.backend,
+                    x1: x,
+                    y1: y + height,
+                    x2: x + width,
+                    y2: y + height,
+                    directions: Meta.BarrierDirection.NEGATIVE_Y
+                });
+
+                this._pressureBarrier.addBarrier(barrier);
+            }
+        }
+    }
+
+    _destroyBarriers() {
+        if (!this._pressureBarrier)
+            return;
+
+        while (this._pressureBarrier._barriers.length > 0) {
+            const barrier = this._pressureBarrier._barriers[0];
+            this._pressureBarrier.removeBarrier(barrier);
+            barrier.destroy();
+        }
+    }
+
+    _destroyPressureBarrier() {
+        this._pressureBarrier?.disconnectObject(this);
+        this._pressureBarrier?.destroy();
+        this._pressureBarrier = null;
+    }
+
+    destroy() {
+        if (this._barriersTimeout)
+            GLib.source_remove(this._barriersTimeout);
+        this._barriersTimeout = null;
+
+        Main.layoutManager.disconnectObject(this);
+
+        this._destroyBarriers();
+        this._destroyPressureBarrier();
+    }
+}
+
+export default class BottomOverviewExtension {
     enable() {
         if (Main.layoutManager._startingUp)
-            Main.layoutManager.connectObject('startup-complete', () => this._initBottomOverview(), this);
+            Main.layoutManager.connectObject('startup-complete',
+                () => { this._bottomOverview = new BottomOverview(); }, this);
         else
-            this._initBottomOverview();
+            this._bottomOverview = new BottomOverview();
     }
 
     disable() {
